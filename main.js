33e36132507e375e112ad773be423d07e2ee681f
Dan Dumont <ddumont@gmail.com>
2014-12-29 19:31:58 -0500

    added hashing to verify files are the same.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 	var fs = require('fs'),
 	    walk = require('walk'),
 	    path = require('path'),
+	    digest = require('digest-stream'),
 	    args = process.argv.slice(2),
 	    root = args.shift();
 
@@ -32,15 +33,56 @@
   });
 
   walker.on("end", function () {
-    Object.keys(map).forEach(function(size) {
-    	var files = map[size];
-    	if (files.length > 1) {
-    		console.log('Duplicates found:');
-    		files.forEach(function(file) {
-    			console.log('  ' + file);
-    		});
-    	}
-    });
-    console.log("all done");
+  	var sizes = Object.keys(map);
+
+    function nextSize() {
+    	var size = sizes.shift();
+    	while (size && map[size].length < 2)
+    		size = sizes.shift();
+    	if (!size)
+    		return console.log("all done");
+
+    	nextFile(size, {});
+    }
+
+    function nextFile(size, summap) {
+    	var file = map[size].shift();
+    	if (!file)
+    		return report(size, summap);
+
+    	var sum,
+			    rstream = fs.createReadStream(file),
+		      dstream = digest('md5', 'hex', function(result) {
+		   	  	sum = result;
+		      });
+       
+			rstream
+			  .pipe(dstream)
+				.on('error', function(error) {
+					console.error(error);
+				})
+				.on('data', function (data) { })
+				.on('end', function() {
+			    if (!summap[sum])
+			    	summap[sum] = [];
+			    summap[sum].push(file);
+			    nextFile(size, summap);
+				});
+    }
+
+    function report(size, summap) {
+    	Object.keys(summap).forEach(function(sum) {
+	    	var files = summap[sum];
+	    	if (files.length > 1) {
+	    		console.log('Duplicates found. size:[' + size + '] sum:['+ sum + ']');
+	    		files.forEach(function(file) {
+	    			console.log('  ' + file);
+	    		});
+	    	}
+	    });
+    	nextSize();
+    }
+
+    nextSize();
   });
 })();
